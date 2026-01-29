3afc1008561114f1def469c07cca1ebcbdec4815
Aryan7384 <aryanamraei6@gmail.com>
2026-01-29 13:37:47 +0330

    added the register-teacher page

--- apps/admin/routes.py
+++ apps/admin/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, url_for, redirect, session, flash, render_template
-from apps.users.models import User, Azmoon, RealQuestion, RealOption
+from apps.users.models import *
 from apps.database import db
 from apps.admin.forms import *
 from ..extensions import hashing
@@ -10,12 +10,11 @@ import dotenv
 dotenv.load_dotenv()
 
 blueprint = Blueprint('admin', __name__)
-get_post = ['GET', 'POST']
 
 
 @blueprint.route("/admin/")
 def admin_homepage():
-    if not session.get("admin_logged_in"):
+    if session.get("admin_logged_in") != True:
         flash("اول رمز عبور را وارد کنید.")
         return redirect(url_for('admin.login'))
     
@@ -39,4 +38,55 @@ def login():
 
 @blueprint.route("/admin/manage-teachers")
 def manageTeachers():
-    return render_template("admin/manage-teachers.html")
+    if session.get("admin_logged_in") != True:
+        flash("اول رمز عبور را وارد کنید.")
+        return redirect(url_for('admin.login'))
+    
+    teachers = []
+    teachers_record = Teacher.query.all()
+
+    for teacher in teachers_record:
+        teacher_dict = {"username": teacher.username,
+                        "students": User.query.where(
+                            User.teacher_id == teacher.id
+                        ).all()[:3],
+                        "id": teacher.id}
+        teachers.append(teacher_dict)
+
+    return render_template("admin/manage-teachers.html",
+                           teachers=teachers)
+
+
+@blueprint.route("/admin/register-teacher", method=["GET", "POST"])
+def registerTeacher():
+    if session.get("admin_logged_in") != True:
+        flash("اول رمز عبور را وارد کنید.")
+        return redirect(url_for('admin.login'))
+    
+    form = RegisterTeacherForm()
+    if form.validate_on_submit():
+        new_teacher = Teacher(username=form.username.data,
+                              password=hashing.hash_value(
+                                  form.password.data,
+                                  salt=os.getenv("SALT")
+                              ))
+        
+        db.session.add(new_teacher)
+        db.session.commit()
+
+        flash("معلم جدید ثبت شد!")
+        return redirect(url_for("admin.admin_homepage"))
+
+    return render_template("admin/resgiter-teacher.html")
+
+
+@blueprint.route("/admin/remove-teacher")
+def removeTeacher():
+    flash("not implemented")
+    return redirect(url_for('admin.admin_homepage'))
+
+
+@blueprint.route("/admin/modify-teacher")
+def modifyTeacher():
+    flash("not implemented")
+    return redirect(url_for('admin.admin_homepage'))
