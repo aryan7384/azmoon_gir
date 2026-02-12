ecad3d77d308f28591d519fc091761ee6ec01abe
aryan7384 <aryanamraei6@gmail.com>
2026-02-12 13:26:16 +0330

    decrease black lines

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -13,7 +13,6 @@ __all__ = ['RegisterForm',
            'PasswordResetForm',]
 
 
-
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
