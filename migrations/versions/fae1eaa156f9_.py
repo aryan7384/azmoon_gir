3f92407d78e6d79871fc0f0137fccb04c8c8a686
aryan7384 <aryanamraei6@gmail.com>
2026-06-22 01:08:23 +0330

    renamed

--- migrations/versions/fae1eaa156f9_.py
+++ migrations/versions/fae1eaa156f9_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: eade79565c18
+Revision ID: fae1eaa156f9
 Revises: 
-Create Date: 2026-05-31 00:48:57.328395
+Create Date: 2026-06-22 00:29:34.156390
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'eade79565c18'
+revision = 'fae1eaa156f9'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -21,6 +21,7 @@ def upgrade():
     op.create_table('teacher',
     sa.Column('username', sa.String(length=50), nullable=False),
     sa.Column('password', sa.String(length=256), nullable=False),
+    sa.Column('student_limit', sa.Integer(), nullable=False),
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('created_at', sa.DateTime(), nullable=True),
     sa.Column('updated_at', sa.DateTime(), nullable=True),
